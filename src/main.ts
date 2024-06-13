3035527a8b17918cf7bd58a8b5de5a6bc3dd8011
sammanamgain <lionalmessi609@gmail.com>
2024-06-13 07:56:45 +0545

    start screen and end screen added

--- src/main.ts
+++ src/main.ts
@@ -15,27 +15,78 @@ window.onload = () => {
   let score = 0;
   const jumpspeed = -12;
   const gravity = 0.4;
+  let gameState = "start";
 
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
 
   let background_image = new Image();
   background_image.src = "./doodlejumpbg.png";
-
   function drawBackground() {
     ctx.drawImage(background_image, 0, 0, canvas.width, canvas.height);
   }
+  function drawStartScreen() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBackground();
+
+    ctx.fillStyle = "red";
+    ctx.font = "48px 'Comic Neue', sans-serif";
+    ctx.fillText("doodle jump", canvasWidth / 2 - 120, 100);
+
+    ctx.fillStyle = "black";
+    ctx.font = "36px 'Comic Neue', sans-serif";
+    ctx.fillText("play", canvasWidth / 2 - 30, 250);
+    ctx.fillStyle = "green";
+    ctx.font = "36px 'Comic Neue', sans-serif";
+    ctx.fillText("press Enter to start", canvasWidth / 2 - 140, 350);
+
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 3;
+    ctx.roundRect(canvasWidth / 2 - 60, 210, 120, 50, 10);
+    ctx.stroke();
+  }
 
   function drawGameOver() {
+    let highscore = localStorage.getItem("doodle_score");
+    let ishighscore = false;
+    highscore = highscore ? parseInt(highscore) : 0;
+    if (score > highscore) {
+      localStorage.setItem("doodle_score", score.toString());
+      ishighscore = true;
+      highscore = score;
+    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBackground();
+
     ctx.fillStyle = "red";
     ctx.font = "48px sans-serif";
     ctx.fillText("Game Over", canvasWidth / 2 - 120, canvasHeight / 2);
+
     ctx.font = "24px sans-serif";
     ctx.fillText(
       "Press Space to Restart",
       canvasWidth / 2 - 110,
       canvasHeight / 2 + 50
     );
+
+    ctx.fillStyle = "gold";
+    ctx.font = "32px sans-serif";
+    ctx.fillText(
+      "High Score: " + highscore,
+      canvasWidth / 2 - 100,
+      canvasHeight / 2 - 100
+    );
+
+    if (ishighscore) {
+      console.log("got high score");
+      ctx.fillStyle = "red";
+      ctx.fillText(
+        "New High Score!",
+        canvasWidth / 2 - 100,
+        canvasHeight / 2 - 50
+      );
+    }
   }
 
   let platforms: Platform[] = [];
@@ -65,7 +116,7 @@ window.onload = () => {
   function newPlatform() {
     let randomX = Math.floor(Math.random() * (canvasWidth - platformWidth));
     let highestPlatform = Math.min(...platforms.map((p) => p.position.y));
-    let newY = highestPlatform - Math.random() * 50 - 50; // 50 to 100 pixels higher
+    let newY = highestPlatform - Math.random() * 50 - 50;
 
     let platform = new Platform(
       new Center(randomX, newY),
@@ -91,6 +142,7 @@ window.onload = () => {
         "./platform.png"
       )
     );
+    gameState = "playing";
   }
 
   let doodle = new Player(
@@ -119,53 +171,57 @@ window.onload = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBackground();
 
-    if (gameover) {
-      drawGameOver();
-    } else {
-      doodle.update();
+    if (gameState === "start") {
+      drawStartScreen();
+    } else if (gameState === "playing") {
+      if (gameover) {
+        drawGameOver();
+      } else {
+        doodle.update();
 
-      // Move platforms down if doodle is going up and in upper half of screen
-      if (doodle.vy < 0 && doodle.position.y < canvasHeight / 2) {
-        let platformSpeed = -doodle.vy;
-        platforms.forEach((p) => {
-          p.position.y += platformSpeed;
-        });
-        doodle.position.y += platformSpeed;
-      }
-
-      // Check for collisions and remove off-screen platforms
-      for (let i = platforms.length - 1; i >= 0; i--) {
-        let p = platforms[i];
-        p.draw();
-
-        if (detectCollision(p, doodle) && doodle.vy >= 0) {
-          doodle.jump();
+        if (doodle.vy < 0 && doodle.position.y < canvasHeight / 2) {
+          let platformSpeed = -doodle.vy;
+          platforms.forEach((p) => {
+            p.position.y += platformSpeed;
+          });
+          doodle.position.y += platformSpeed;
         }
 
-        if (p.position.y > canvasHeight) {
-          platforms.splice(i, 1);
-          newPlatform();
-          score++;
+        for (let i = platforms.length - 1; i >= 0; i--) {
+          let p = platforms[i];
+          p.draw();
+
+          if (detectCollision(p, doodle) && doodle.vy >= 0) {
+            doodle.jump();
+          }
+
+          if (p.position.y > canvasHeight) {
+            platforms.splice(i, 1);
+            newPlatform();
+            score++;
+          }
+        }
+
+        doodle.draw();
+
+        if (doodle.position.y > canvasHeight) {
+          gameover = true;
         }
       }
 
-      doodle.draw();
-
-      if (doodle.position.y > canvasHeight) {
-        gameover = true;
-      }
+      ctx.fillStyle = "blue";
+      ctx.font = "16px sans-serif";
+      ctx.fillText(`Score: ${score}`, 5, 20);
     }
-
-    ctx.fillStyle = "blue";
-    ctx.font = "16px sans-serif";
-    ctx.fillText(`Score: ${score}`, 5, 20);
 
     requestAnimationFrame(update);
   }
   update();
 
   document.addEventListener("keydown", (e) => {
-    if (e.key === " " && gameover) {
+    if (e.key === "Enter" && gameState === "start") {
+      resetGame();
+    } else if (e.key === " " && gameState === "playing" && gameover) {
       resetGame();
     }
   });
